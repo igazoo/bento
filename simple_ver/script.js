80ac143ba326044e257812457ffba243b67ff4b4
mayasan-sc <mayasan.sc@gmail.com>
2019-11-16 00:01:37 +0900

    echanged anonymous functionns to arrow functions

--- simple_ver/script.js
+++ simple_ver/script.js
@@ -4,39 +4,41 @@
 
 //表示切り替え
 
-//定数
 const name = document.getElementById('name');
 const price = document.getElementById('price');
 const image = document.getElementById('image');
 
 //並
-document.getElementById('nami').onclick = function(){
+const nami = document.getElementById('nami');
+
+nami.onclick = ()=>{
   name.textContent = "お弁当 -並-";
   price.textContent = "1000";
   image.src = "img/nami.png";
 }
 
-document.getElementById('nami').addEventListener('mouseenter', ()=>{
+nami.addEventListener('mouseenter', ()=>{
   nami.classList.add('btn-lg');
 },false);
 
-document.getElementById('nami').addEventListener('mouseleave', ()=>{
+nami.addEventListener('mouseleave', ()=>{
   nami.classList.remove('btn-lg');
 },false);
 
 
 //上
-document.getElementById('jou').onclick = function(){
+const jou = document.getElementById('jou');
+jou.onclick = ()=>{
   name.textContent = "お弁当 -上-";
   price.textContent = "2000";
   image.src = "img/jou.png";
 }
 
-document.getElementById('jou').addEventListener('mouseenter', ()=>{
+jou.addEventListener('mouseenter', ()=>{
   jou.classList.add('btn-lg');
 },false);
 
-document.getElementById('jou').addEventListener('mouseleave', ()=>{
+jou.addEventListener('mouseleave', ()=>{
   jou.classList.remove('btn-lg');
 },false);
 
@@ -44,7 +46,8 @@ document.getElementById('jou').addEventListener('mouseleave', ()=>{
 
 //ダイアログ
 //アラートダイアログ
-let hour = new Date().getHours();
+const hour = new Date().getHours();
+//console.log(hour);
 if (hour < 16){
   window.alert('朝と昼は並弁当が売れてます！');
 } else {
@@ -53,7 +56,7 @@ if (hour < 16){
 
 //カート確認ダイアログ
 const sumPrice = [];
-document.getElementById('cart-confirm').onclick = function(){
+document.getElementById('cart-confirm').onclick = ()=>{
   const cart = window.confirm(`${name.textContent} をカートに入れますか？` );
   const li = `<li>${name.textContent}</li>`;
 
@@ -71,13 +74,42 @@ document.getElementById('cart-confirm').onclick = function(){
   }
 }
 
+//json ファイルからの在庫状況の取得 (Webサーバー経由で動作)
+const stockArray = [];
+$(document).ready(()=>{
+  $.ajax({url:'stock.json', dataType:'json'})
+  .done((data)=>{
+    data.forEach((item, index)=>{
+      stockArray.push(item.num);
+    })
+  })
+})
+
+console.log(stockArray);
+
+//在庫確認文字列ダイアログ (Webサーバー経由で動作)
+document.getElementById('stock-confirm').onclick = ()=>{
+  const stockConfirm = window.prompt(`${name.textContent} の在庫を確認しますか？ yes または no でお答えください。` );
+  if (stockConfirm === 'yes'){
+    console.log(name.textContent);
+    switch(name.textContent) {
+      case 'お弁当 -並-' : window.alert(`在庫は ${stockArray[0]} 個です。`);
+      break;
+      case 'お弁当 -上-' : window.alert(`在庫は ${stockArray[1]} 個です。`);
+      break;
+    }
+  } else {
+    window.alert(`在庫の確認をキャンセルしました`)
+  }
+}
+
 
 
 //その他メニューの折りたたみ
-$(document).ready(function(){
+$(document).ready(()=>{
   let hide = 0;
 
-  $('#hide').on('click', function(){
+  $('#hide').on('click', ()=>{
     if (hide === 0) {
       $('#cartlist li').addClass('d-none');
       $('#hide').text('表示');
@@ -93,65 +125,71 @@ $(document).ready(function(){
 
 
 //フォーム入力値の取得
+
+const optionExplanation = document.getElementById('optionExplanation');
+
 //ラジオボタンの入力値を取得して表示
-document.getElementById('radioForm').onsubmit = function(event){
+
+const radioForm = document.getElementById('radioForm');
+radioForm.onsubmit = (event)=>{
   event.preventDefault();
 
   let radio;
-  radio = document.getElementById('radioForm').option.value;
-  document.getElementById('optionExplanation').textContent = `ご飯の量 : ${radio}`;
+  radio = radioForm.option.value;
+  optionExplanation.textContent = `ご飯の量 : ${radio}`;
 };
 
 //チェックボックスの入力値を取得して表示
-document.getElementById('checkboxForm').onsubmit = function(event){
+document.getElementById('checkboxForm').onsubmit = (event)=>{
   event.preventDefault();
 
-  let elements = document.getElementsByName('accessory');
-  let checkbox = "付属品 :";
-  let i=0;
+  const elements = document.getElementsByName('accessory');
+  const checkbox = "付属品 :";
+  const i=0;
   while(i < 3){
     if(elements[i].checked) checkbox = checkbox + ' ' + elements[i].value;
     i += 1;
   };
-  document.getElementById('optionExplanation').textContent = checkbox;
+  optionExplanation.textContent = checkbox;
 };
 
 //セレクトドロップダウンの入力値を取得して表示
-document.getElementById('selectForm').onsubmit = function(event){
+const selectForm = document.getElementById('selectForm');
+selectForm.onsubmit = (event)=>{
   event.preventDefault();
-  const option = document.getElementById('selectForm').select.value;
-  document.getElementById('optionExplanation').textContent = `当店を知ったきっかけ : ${option}`;
+  const option = selectForm.select.value;
+  optionExplanation.textContent = `当店を知ったきっかけ : ${option}`;
 };
 
 //テキストボックスの入力値を取得して表示
 //Google Maps API を利用し入力した値に該当する地図を表示
-document.getElementById('textForm').onsubmit = function(event){
+document.getElementById('textForm').onsubmit = (event)=>{
   event.preventDefault();
   const text = document.getElementById('textForm').text.value;
   const map = `<br><iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC8F2vD0OFURJR3LovVPmhNjTUkNOswwZE&q=${text}"  width="300" height="225" frameborder="0" style="border:0;" allowfullscreen=""></iframe>`;
-  document.getElementById('optionExplanation').textContent = `お届け先住所 : ${text}`;
-  document.getElementById('optionExplanation').insertAdjacentHTML('beforeend', map);
+  optionExplanation.textContent = `お届け先住所 : ${text}`;
+  optionExplanation.insertAdjacentHTML('beforeend', map);
 };
 
 //テキストエリアボックスの入力値を取得して表示
-document.getElementById('textareaForm').onsubmit = function(event){
+document.getElementById('textareaForm').onsubmit = (event)=>{
   event.preventDefault();
   const textarea = document.getElementById('textareaForm').textarea.value;
-  document.getElementById('optionExplanation').textContent = `その他ご要望 : ${textarea}`;
+  optionExplanation.textContent = `その他ご要望 : ${textarea}`;
 };
 
 
+
 //閉店カウントダウン
-setInterval(function () {
-   let now =  moment();
-   let goal = new Date();
+setInterval(()=> {
+   const now =  moment();
+   const goal = new Date();
    goal.setHours('23');
    goal.setMinutes('59');
    goal.setSeconds('59');
    goal = moment(goal);
 
-   let diff = goal.diff(now);
-   let duration = moment.duration( diff );
+   const diff = goal.diff(now);
+   const duration = moment.duration( diff );
    document.getElementById('countdown').textContent = `本日閉店まで ${duration.hours()} 時間 ${duration.minutes()} 分 ${duration.seconds()} 秒`;
-   console.log(goal);
 }, 1000);
